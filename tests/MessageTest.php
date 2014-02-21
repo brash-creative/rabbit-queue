34db4d3d14007bb6a68f974eb09c05f2dbb4bc4c
crashleyp <paulcrashley@rentalcars.com>
2014-02-21 16:46:45 +0000

    Tests

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -1,13 +1,23 @@
 <?php
+namespace RabbitQueueTest;
 
 use RabbitQueue\Services\Message;
 
-class MessageTest extends PHPUnit_Framework_TestCase {
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
     public function testStringException()
     {
         $this->setExpectedException('RabbitQueue\Exceptions\MessageException');
 
         $message    = new Message();
         $message->setPayload(array('test'=>1));
+    }
+
+    public function testIsString()
+    {
+        $message    = new Message();
+        $message->setPayload('test');
+
+        $this->assertTrue(is_string((string) $message));
     }
 }
--- tests/PublishTest.php
+++ tests/PublishTest.php
@@ -1,16 +1,52 @@
 <?php
-use RabbitQueue\Services\Message;
-use PhpAmqpLib\Connection\AMQPConnection;
-use RabbitQueue\Services\Publish;
+namespace RabbitQueueTest;
 
-class PublishTest extends PHPUnit_Framework_TestCase {
+use RabbitQueue\Services\Message;
+
+class PublishTest extends \PHPUnit_Framework_TestCase
+{
     public function testNoQueueException()
     {
         $this->setExpectedException('RabbitQueue\Exceptions\PublishException');
 
+        $amqp       = $this->getMockBuilder('PhpAmqpLib\Connection\AMQPConnection')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $service    = \Mockery::mock('RabbitQueue\Services\Publish[getChannel]', array($amqp));
+        $channel    = $this->getMockBuilder('PhpAmqpLib\Channel\AMQPChannel')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+
+        $channel->expects($this->never())
+                ->method('basic_publish')
+                ->will($this->returnValue(true));
+
         $message    = new Message();
-        $amqp       = new AMQPConnection('rabbitmq-test.server.traveljigsaw.com', 5672, 'guest', 'guest');
-        $publish    = new Publish($amqp);
-        $publish->publish($message);
+
+        $service->shouldReceive('getChannel')->never();
+
+        $service->publish($message);
+    }
+
+    public function testPublish()
+    {
+        $message    = new Message();
+        $message->setQueue('test')->setPayload('test');
+
+        $amqp       = $this->getMockBuilder('PhpAmqpLib\Connection\AMQPConnection')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $service    = \Mockery::mock('RabbitQueue\Services\Publish[getChannel]', array($amqp));
+        $channel    = $this->getMockBuilder('PhpAmqpLib\Channel\AMQPChannel')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+
+        $channel->expects($this->once())
+                ->method('basic_publish')
+                ->will($this->returnValue(true));
+
+        $service->shouldReceive('getChannel')->once()->andReturn($channel);
+
+        $service->publish($message);
     }
 }
